4d799a7b8f9f8b93cafd836c734d896b085f8f94
Guilherme Seiti <gseitihomma@gmail.com>
2020-10-15 09:58:52 -0300

    implemented inputs submit to server

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useState, ChangeEvent } from "react";
+import { useHistory } from "react-router-dom";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 
@@ -8,8 +9,12 @@ import Sidebar from '../components/Sidebar';
 import mapIcon from "../utils/mapIcon";
 
 import '../styles/pages/create-orphanage.css';
+import api from "../services/api";
+
 
 export default function CreateOrphanage() {
+  const history = useHistory();
+
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
   const [name, setName] = useState('');
@@ -45,23 +50,28 @@ export default function CreateOrphanage() {
     setPreviewImages(selectedImagesPreview);
   }
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
-    const { latitude, longitude } = position
+    const { latitude, longitude } = position;
 
-    console.log(
-      {
-        name,
-        about,
-        latitude,
-        longitude,
-        instructions,
-        opening_hours,
-        open_on_weekends
-      }
-    )
+    const data = new FormData();
 
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach(image => data.append('images', image));
+
+    await api.post('orphanages', data);
+
+    alert("Cadastro realizado com sucesso!");
+
+    history.push('/app');
   }
 
   return (
@@ -74,7 +84,7 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[-23.5247764, -46.2023049]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onClick={handleMapClick}
